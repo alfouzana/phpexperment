1bfbc6f5bee664987e797be2f487c20afa7c15ca
Abdullah <alfouzana@me.com>
2021-03-18 00:19:56 +0300

    good luck

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('addproduct', function () {
+Route::get('/addproduct', function () {
     return view('addproduct');
 });
 
