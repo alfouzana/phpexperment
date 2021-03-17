a58c06675b5537b252e4899ff64a88f66603f9e6
Abdullah <alfouzana@me.com>
2021-03-18 01:37:35 +0300

    add product form

--- resources/views/addproduct.blade.php
+++ resources/views/addproduct.blade.php
@@ -2,15 +2,44 @@
 
 <div class="row">
 
+<form action="submit" method="post">
+
 <div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Email address</label>
-  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-</div>
-<div class="mb-3">
-  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  <label for="exampleFormControlInput1" class="form-label">إسم المنتج</label>
+  <input class="form-control" id="exampleFormControlInput1"name="productname" placeholder="إسم المنتج">
 </div>
 
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">سعر المنتج</label>
+  <input class="form-control" id="exampleFormControlInput1" name="price" placeholder="سعر المنتج">
 </div>
 
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">رابط المنتج</label>
+  <input class="form-control" id="exampleFormControlInput1" name="link" placeholder="الرابط">
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">رابط الصورة</label>
+  <input class="form-control" id="exampleFormControlInput1" name="imgurl" placeholder="الرابط">
+</div>
+
+<div class="mb-3">
+<label for="exampleDataList" class="form-label">القسم</label>
+<input class="form-control" list="datalistOptions" id="exampleDataList" name="cat" placeholder="Type to search...">
+<datalist id="datalistOptions">
+  <option value="الإلكترونيات">
+  <option value="الملابس">
+  <option value="الإنارة">
+  <option value="مستلزمات المنزل">
+  <option value="تحف">
+</datalist>
+</div>
+
+<div class="mb-3">
+<button type="button" class="btn btn-primary btn-lg">إضافة المنتج</button>
+</div>
+</form>
+
+</div>
 @include('partials.footer')
