2a844104f5a463ba28d48fccc32cce824d084ff1
Abdullah <alfouzana@me.com>
2021-03-18 02:29:59 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\products;
+use app\Http\Controllers\products;
 
 /*
 |--------------------------------------------------------------------------
