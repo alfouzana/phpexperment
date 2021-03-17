78f3558b5fda12fdcf6553cb9b8599aada8cf53c
Abdullah <alfouzana@me.com>
2021-03-17 23:38:53 +0300

    change web

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/addproduct', function () {
+    return view('addproduct');
 });
 
