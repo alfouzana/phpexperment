41b4d7a134d8f00e12354630fe7026d079072273
Abdullah <alfouzana@me.com>
2021-03-18 00:18:15 +0300

    changes

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/phpProject/addproduct', function () {
+Route::get('addproduct', function () {
     return view('addproduct');
 });
 
