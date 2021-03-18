f76b1e642d7a4169d09c8da94eb8f2f7834308cb
Abdullah <alfouzana@me.com>
2021-03-19 02:56:43 +0300

    blogadd

--- resources/views/addblog.blade.php
+++ resources/views/addblog.blade.php
@@ -3,17 +3,25 @@
 
 <form action="submit" method="post">
 <input type="hidden" name="method" value="PUT">
-<input type="hidden" name="_token" value="{{ csrf_token() }}">"
+<input type="hidden" name="_token" value="{{ csrf_token() }}">
 @csrf
 
 <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">عنوان المدونة</label>
   <input class="form-control form-control-lg" name="blogtitle" type="text" placeholder="عنوان المدونة" aria-label=".form-control-lg example">
 </div>
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">صورة العرض</label>
+  <input class="form-control form-control-lg" name="imgurl" type="text" placeholder="صورة العرض" aria-label=".form-control-lg example">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">مقدمة المدونة</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1"  name="caption" rows="3"></textarea>
+</div>
 
 <div class="mb-3">
   <label for="exampleFormControlInput1" class="form-label">تفاصيل المدونة</label>
-  <textarea id="summernote" name="blogpost"></textarea></div>
+  <textarea id="summernote"  rows="3" name="blogpost"></textarea></div>
   <div class="mb-3">
   <button class="btn btn-primary" type="submit">إضافة مدونة</button>
   </div>
