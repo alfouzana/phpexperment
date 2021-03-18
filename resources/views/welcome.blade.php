b280f08f384470346f5a9e3bd6c8e92b84239595
Abdullah <alfouzana@me.com>
2021-03-18 21:07:50 +0300

    late

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,16 +6,16 @@
       @foreach ($affP as $product)
         <div class="col">
           <div class="card shadow-sm">
-          <img src="{{$product['imagelink']}}" class="card-img-top" alt="Girl in a jacket">
+          <img src="{{$product->imagelink}" class="card-img-top" alt="Girl in a jacket">
 
             <div class="card-body">
-            <h5 class="card-title">{{$product['name']}}</h5>
-              <p class="card-text">{{$product['category']}}</p>
+            <h5 class="card-title">{{$product->name}}</h5>
+              <p class="card-text">{{$product[->category}}</p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
-                 <a href="{{$product['productlink']}}" class="btn btn-sm btn-outline-secondary">إشتريه من علي إكسبريس</a> 
+                 <a href="{{$product->productlink}" class="btn btn-sm btn-outline-secondary">إشتريه من علي إكسبريس</a> 
                 </div>
-                <small class="text-muted">{{$product['price']}}</small>
+                <small class="text-muted">{{$product->price}}</small>
               </div>
             </div>
           </div>
