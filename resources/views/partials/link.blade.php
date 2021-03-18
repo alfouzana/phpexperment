74974aa2b2410ba378fbda902bf09387a36d599b
Abdullah <alfouzana@me.com>
2021-03-18 18:45:11 +0300

    upload

--- resources/views/partials/link.blade.php
+++ resources/views/partials/link.blade.php
@@ -1,6 +1,6 @@
 <ul class="list-unstyled">
             <li><a href="/addproduct" class="text-white">اضف منتج</a></li>
             @foreach ($uniqueCollection as $product)
-            li><a href="/{{$product['category']}}" class="text-white">{{$product['category']}}</a></li>
+            <li><a href="/{{$product['category']}}" class="text-white">{{$product['category']}}</a></li>
             @endforeach
           </ul>
