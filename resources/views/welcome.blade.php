685f22f5682f87d26302c9cf45bea558a228cf74
Abdullah <alfouzana@me.com>
2021-03-18 15:50:26 +0300

    testing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 @include('partials.header')
 
+{{$products}}
+     
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
       @foreach ($products as $product)
         <div class="col">
