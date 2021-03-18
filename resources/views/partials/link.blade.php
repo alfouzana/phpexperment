4da59d6afdc304d0a247c06bebb86bc99b11780a
Abdullah <alfouzana@me.com>
2021-03-18 21:30:52 +0300

    link white

--- resources/views/partials/link.blade.php
+++ resources/views/partials/link.blade.php
@@ -8,12 +8,12 @@
     <a class="nav-link active" aria-current="page" href="#">Active</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link text-white" href="#">Link</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
+    <a class="nav-link text-white" href="#">Link</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+    <a class="nav-link text-white" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
   </li>
 </ul>
