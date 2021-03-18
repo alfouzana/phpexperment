d3ec30bf31eee8fdf3a3b5d7635ffb23c202da35
Abdullah <alfouzana@me.com>
2021-03-18 14:42:09 +0300

    testing

--- app/Http/Controllers/homeProdcutsControllers.php
+++ app/Http/Controllers/homeProdcutsControllers.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
+use App\product;
 class homeProdcutsControllers extends Controller
 {
     //
 
     function show()
     {
+
+        $products = product::all();
+        dd($products);
         return view('welcome');
     }
 }
