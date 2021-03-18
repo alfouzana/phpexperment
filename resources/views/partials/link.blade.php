9c95a9fbde2b0ce6f46a49c143c0df5d9248fca8
Abdullah <alfouzana@me.com>
2021-03-18 21:29:12 +0300

    links

--- resources/views/partials/link.blade.php
+++ resources/views/partials/link.blade.php
@@ -2,3 +2,18 @@
             <li><a href="/addproduct" class="text-white">اضف منتج</a></li>
          
           </ul>
+
+          <ul class="nav flex-column">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
