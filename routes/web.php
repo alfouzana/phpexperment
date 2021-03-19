a301db9b48661ffc80e9b6dfc6b0df21764b1812
Abdullah <alfouzana@me.com>
2021-03-19 16:29:44 +0300

    wow

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,6 @@ Route::get('/article/{name}', [blogController::class, 'show', '$name']);
 
 
 
-
-
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
