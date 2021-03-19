d35afc8d3cc5eb2d93559f22b72496abbb9dbc29
Abdullah <alfouzana@me.com>
2021-03-19 04:14:32 +0300

    lets test

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,12 +7,13 @@
   <div class="card-body">
     <h5 class="card-title">{{$blog->blogtitle}}</h5>
     <h6 class="card-subtitle mb-2 text-muted">عبدالله الفوزان</h6>
-    {{$blog->blogpost}}
+    
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
   </div>
 </div>
 
+{{$blog->blogpost}}
 
           @endforeach
 
