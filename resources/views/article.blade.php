0ae30bdca0359439ed5f94da3942ef761d647413
Abdullah <alfouzana@me.com>
2021-03-19 04:29:17 +0300

    not brackets

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,7 +7,7 @@
   <div class="card-body">
     <h5 class="card-title">{{$blog->blogtitle}}</h5>
     <h6 class="card-subtitle mb-2 text-muted">عبدالله الفوزان</h6>
-    <p class="card-text"> {{!!$blog->blogpost!!}} </p>
+    <p class="card-text"> {!!$blog->blogpost!!} </p>
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
   </div>
