4746561f8c890d7d0b3d158946e402fa1db13a04
Abdullah <alfouzana@me.com>
2021-03-19 04:27:34 +0300

    blogtime

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -7,7 +7,7 @@
   <div class="card-body">
     <h5 class="card-title">{{$blog->blogtitle}}</h5>
     <h6 class="card-subtitle mb-2 text-muted">عبدالله الفوزان</h6>
-    {{!! $blog->blogpost !!}}
+    <p class="card-text"> {{!!$blog->blogpost!!}} </p>
     <a href="#" class="card-link">Card link</a>
     <a href="#" class="card-link">Another link</a>
   </div>
