e7d7b58268d891df62c76781b9a3e7859f03d0a5
Abdullah <alfouzana@me.com>
2021-03-19 04:41:03 +0300

    awesome

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,7 @@
             <div class="card">
               <div class="row g-0">
                 <div class="col-md-4">
-                <img src="{{$blog->imgurl}}" class="card-img-top" alt="">
+                <img src="{{$blog->imgurl}}" class="card-img-top" style="width: 200px; height: 300px" alt="">
 
                 </div>
                 <div class="col-md-8">
@@ -19,7 +19,7 @@
               </div>
             </div>
           </div>
-</div>
+
 
           @endforeach
 
